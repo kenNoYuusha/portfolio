9f2b3abf090c41bd9e77d71d16953062b2823926
Jluis <jorge_luis1709@hotmail.com>
2024-11-22 21:09:05 -0600

    chore: setup env

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '../app/layout/layout.component';
+import { HomeComponent } from '../app/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
